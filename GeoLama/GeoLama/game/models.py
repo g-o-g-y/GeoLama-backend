4cc3373964969a70d4b61351c3b61546426575de
g-o-g-y <larkin.cema@outlook.com>
2022-02-09 18:35:02 +0300

    basic_authentication_implementation

--- GeoLama/GeoLama/game/models.py
+++ GeoLama/GeoLama/game/models.py
@@ -2,20 +2,18 @@ from django.db import models
 from django.urls import reverse  # To generate URLS by reversing URL patterns
 
 
-class Users(models.Model):
+class Raining(models.Model):
     """
     A typical class defining a model, derived from the Model class.
     """
 
     # Fields
-    user_id = models.AutoField(max_length=100, null=False, primary_key=True)
-    user_name = models.CharField(max_length=100)
-    user_points = models.IntegerField(max_length=20)
-    user_email = models.EmailField(max_length=100)
+    user_id = models.IntegerField(null=False, primary_key=True)
+    user_points = models.IntegerField(default=0)
     # Metadata
-    # class Meta:
+    class Meta:
     # сортировка
-        # ordering = ["-my_field_name"]
+        ordering = ["user_points"]
         # ordering = ["title", "-pubdate"]
     # Methods
 
